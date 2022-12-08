26891b1a73675c08e5859bf8af58383303c8e894
Jeffrey Shimbo <jts@local>
2022-12-08 00:47:17 -0800

    Update tests

--- tests/test_tone.py
+++ tests/test_tone.py
@@ -1,6 +1,7 @@
 from chirp_convert.main import *
 
-def test100():
+
+def test_tone():
     assert process_tone("100Hz") == ('Tone', 100, 88.5)
     assert process_tone("100 Hz") == ('Tone', 100, 88.5)
     assert process_tone("99 HZ") == ('Tone', 99, 88.5)
@@ -11,10 +12,16 @@ def test100():
     assert process_tone(" ") == ('', 88.5, 88.5)
     assert process_tone("Hz") == ('', 88.5, 88.5)
 
+
 def test_frequency():
-    assert process_frequency("100") == (0, '', 0)
-    assert process_frequency("100.0") == (100, '', 0)
-    assert process_frequency("144.0") == (144.0, '', 0)
+    assert process_frequency("") == (0, '', 0)
+    assert process_frequency(" ") == (0, '', 0)
+    assert process_frequency("100") == (100, '', 0)
+    assert process_frequency(" 100") == (100, '', 0)
+    assert process_frequency("100.0 ") == (100, '', 0)
+    assert process_frequency(" 100.0 ") == (100, '', 0)
+    assert process_frequency(" 100. ") == (100, '', 0)
+    assert process_frequency("144.0") == (144, '', 0)
     assert process_frequency("144.9+") == (144.9, '+', 0.6)
     assert process_frequency("144.9-") == (144.9, '-', 0.6)
     assert process_frequency("444.9+") == (444.9, '+', 5)
